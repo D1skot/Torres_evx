4d8fd378cfaa0991b541df9b1445754830bcebce
D1skot <131003063+D1skot@users.noreply.github.com>
2024-05-07 23:00:01 +0200

    +

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -1,4 +1,16 @@
 @extends('page.layout')
 @section('content')
- SIEMA
+ <img class="img-fluid w-100" src="{{asset('img/banner.webp')}}" alt="Torres EVX">
+<div class="container-fluid">
+   <h1 class="text-center main_color fw-bold my-lg-5 my-3">Poznaj szczegóły wydarzenia</h1>
+
+   <div class="d-flex">
+       <a href="#">
+           <div class="kafla"><img src="{{asset('img/kafla.webp')}}" alt=""></div>
+       </a>
+       <a href="#">
+           <div class="kafla"><img src="{{asset('img/kafla.webp')}}" alt=""></div>
+       </a>
+   </div>
+</div>
 @endsection
--- resources/views/page/layout.blade.php
+++ resources/views/page/layout.blade.php
@@ -12,7 +12,37 @@
 </head>
 
 <body>
+    <header>
+        <div class="bg-white px-lg-5 px-3 py-3">
+            <img class="img-fluid" src="{{asset('img/logo.webp')}}" alt="Torres EVX">
+        </div>
+        <nav class="navbar navbar-expand-lg px-lg-5 px-3">
+            <div class="container-fluid">
+                <button class="navbar-toggler border-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Agenda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Mapa obiektu</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Kontakt</a>
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
     @yield('content')
 </body>
 <script src="{{ asset('js/app.js') }}?{{ date('YmdHis') }}"></script>
+
 </html>
